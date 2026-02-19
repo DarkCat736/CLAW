62bc00a34a42d62142688878bcd1a397877c9435
Kitten Tech <104227728+DarkCat736@users.noreply.github.com>
2026-02-18 19:10:53 -0600

    Prepare branch for push to prod

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# CLAW - <i>v0.2.1 (BETA VERSION)</i>
-A platform for students to stay organized and work together! Currently CLAW has a fully functional checklist, an assignment tracker in development, and many more features including Team Projects on the way in the future!
+# CLAW - <i>v0.2.1</i>
+A platform for students to stay organized and work together! Currently, CLAW has a fully functional checklist, an assignment tracker in development, and many more features including Team Projects on the way in the future!
 
-<b><i>UPDATE NOTICE!!!</i></b> The MYSQL_DB_HOST and MYSQL_DB_NAME fields are now required in the .env file for the BETA version of CLAW. Check the BETA setup instructions for more details.
+<b><i>UPDATE NOTICE!!!</i></b> The MYSQL_DB_HOST and MYSQL_DB_NAME fields are now required in the .env file for the PROD version of CLAW. Check the PROD setup instructions for more details.
 
-<b><i>UPDATE NOTICE!!!</i></b> The `info` column on the `accounts` table has been changed to be a `JSON` column. This needs to be updated for data to be properly exchanged.  Check the BETA setup instructions for more details. In addition to this, all data stored for the checklist service is now unreadable by the program. All user inputted data needs to be formatted in Base64. You can either restart checklists and enter data again, use [this](https://github.com/DarkCat736/CLAW/wiki/CLAW-Utility-Scripts#update_checklist_user_data_to_base64js) helper script, or manually go through and translate the user inputted fields in the JSON to Base64.
+<b><i>UPDATE NOTICE!!!</i></b> The `info` column on the `accounts` table has been changed to be a `JSON` column. This needs to be updated for data to be properly exchanged.  Check the PROD setup instructions for more details. In addition to this, all data stored for the checklist service is now unreadable by the program. All user inputted data needs to be formatted in Base64. You can either restart checklists and enter data again, use [this](https://github.com/DarkCat736/CLAW/wiki/CLAW-Utility-Scripts#update_checklist_user_data_to_base64js) helper script, or manually go through and translate the user inputted fields in the JSON to Base64.
 
-<i>Official BETA host: [YOUR DATA IS NOT GUANTEED AT THIS SITE! Use at your own risk.](https://beta.claw.kittentech.org)</i>
+<i>Official host: https://claw.kittentech.org</i>
 
 <i>If the official host is inaccessible please wait at least 20 minutes before making a bug report. The server automatically restarts after 15 minutes of network failures which generally fixes most connection issues.</i>
 
@@ -13,7 +13,30 @@ A platform for students to stay organized and work together! Currently CLAW has 
 
 <b>Setup:</b>
 
-[This guide](https://github.com/DarkCat736/CLAW/wiki/CLAW-BETA-Setup) has setup instructions.
+[This guide](https://github.com/DarkCat736/CLAW/wiki/CLAW-Setup) has detailed setup instructions. Below is a quick overview.
+
+1. Install MySQL server (https://dev.mysql.com/doc/mysql-installation-excerpt/8.0/en/)
+2. Run `CREATE DATABASE claw;USE claw;CREATE TABLE accounts (email text, password text, name text, info json);SET GLOBAL wait_timeout = 2;` in the MySQL console
+3. Create the "claw_user" account using `CREATE USER 'claw_user'@'localhost' IDENTIFIED BY 'claw_password';GRANT ALL PRIVILEGES ON claw.* TO 'claw_user'@'localhost';FLUSH PRIVILEGES;` <i>NOTE: You should change 'claw_password' to a more secure password. This is just an example.</i>
+4. Download the repository and run `npm install`
+5. Follow the instructions below to create a `.env` file
+6. Start server with `node index.js`
+7. Go to `localhost:9876` and you're all set!
+
+<b>.env File Creation Instructions:</b>
+
+1. Create a file called `.env` in the root CLAW directory
+2. Five fields are required - MYSQL_DB_USER, MYSQL_DB_PASSWORD, MYSQL_DB_HOST, MYSQL_DB_NAME, and SERVICE_AVAILABILITY
+3. The two MySQL fields are strings, but the Service Availability field is a stringified JSON!
+4. Here is an example of a fully completed `.env`:
+
+```dotenv
+MYSQL_DB_USER=claw_user
+MYSQL_DB_PASSWORD=claw_password
+MYSQL_DB_HOST=localhost
+MYSQL_DB_NAME=claw
+SERVICE_AVAILABILITY={"checklist":{"available":true},"account":{"available":true}}
+```
 
 <hr>
 <i>WARNING: If you host CLAW using a program such as apache, you may have to add to your VirtualHost configuration to allow encoded slashes in URLs (Example fix: `AllowEncodedSlashes NoDecode`). CLAW's authorization API <b>WILL NOT WORK</b> and will return 404 errors without these changes.</i>
--- service_modules/realtime_handler.js
+++ service_modules/realtime_handler.js
@@ -12,122 +12,7 @@ let api = {
         io = new Server(expressServer);
         ioHandlers();
     },
-    assignment_tracker: {
-
-    },
     onlineUsers: {}
-}
-
-async function assignment_tracker_DIRECTOR(payload, socket) {
-    switch (payload.payload) {
-        case "INIT":
-            try {
-                let authorized = await auth_backend.authAPI.signInForService(payload.email, payload.password, db.dbPool);
-                if (!authorized) {
-                    return {status: "error", code: "Authorization error"};
-                }
-                api.onlineUsers[`${socket.id}`] = {email: payload.email, id: socket.id, service: "assignment_tracker"};
-                console.log(`[${socket.id}] registered as "${payload.email}" for "assignment_tracker"`);
-                return {status: "ok"}
-            } catch(e) {
-                console.log(e);
-                return {status: "error", code: "Init error"};
-            }
-            break;
-        case "GET_AVAILABLE_CLASSES":
-            if (api.onlineUsers[socket.id] !== undefined) {
-                try {
-                    let [rows, fields] = await dbPool.query(`SELECT * FROM accounts WHERE email = '${api.onlineUsers[socket.id].email}';`);
-
-                    let userInfoObject = JSON.parse(rows[0]["info"]);
-                    console.log(userInfoObject);
-                    if (userInfoObject.assignment_tracker === undefined) {
-                        let [rows, fields] = await dbPool.query(`INSERT INTO assignment_tracker VALUES (NULL, '${api.onlineUsers[socket.id].email}', '["${api.onlineUsers[socket.id].email}"]', '{"metadata":{"name":"Untitled 01"}}');`);
-                        userInfoObject.assignment_tracker = {
-                            "0": {
-                                "id": `${rows["insertId"]}`,
-                                "owned": "true"
-                            }
-                        }
-                        console.log(userInfoObject);
-                        console.log(JSON.stringify(userInfoObject));
-                        const result = await dbPool.query(`UPDATE accounts SET info = '${JSON.stringify(userInfoObject)}' WHERE email = '${api.onlineUsers[socket.id].email}';`);
-                        return {status: "ok", payload: userInfoObject.assignment_tracker};
-                    } else {
-                        return {status: "ok", payload: userInfoObject.assignment_tracker};
-                    }
-                } catch(e) {
-                    console.log(e);
-                    return {status: "error", code: "Unknown error"};
-                }
-            } else {
-                return {status: "error", code: "Auth error"};
-            }
-            break;
-        case "GET_CLASS_METADATA":
-            if (api.onlineUsers[socket.id] !== undefined) {
-                try {
-                    let [rows, fields] = await dbPool.query(`SELECT * FROM assignment_tracker WHERE id = ${payload.classId};`);
-                    let permittedUsersForClass = JSON.parse(rows[0]["permittedUsers"]);
-                    if (permittedUsersForClass.includes(api.onlineUsers[socket.id].email)) {
-                        let metadata = JSON.parse(rows[0]["data"]).metadata;
-                        return {status: "ok", payload: metadata};
-                    }
-                    return {status: "error", code: "Auth error"};
-                } catch(e) {
-                    console.log(e);
-                    return {status: "error", code: "Unknown error"};
-                }
-            } else {
-                return {status: "error", code: "Auth error"};
-            }
-            break;
-        case "CREATE_NEW_CLASS":
-            if (api.onlineUsers[socket.id] !== undefined) {
-                try {
-                    let [rows, fields] = await dbPool.query(`SELECT * FROM accounts WHERE email = '${api.onlineUsers[socket.id].email}';`);
-
-                    let userInfoObject = JSON.parse(rows[0]["info"]);
-                    console.log(userInfoObject);
-
-                    [rows, fields] = await dbPool.query(`INSERT INTO assignment_tracker VALUES (NULL, '${api.onlineUsers[socket.id].email}', '["${api.onlineUsers[socket.id].email}"]', '{"metadata":{"name":"Untitled class"}}');`);
-                    userInfoObject.assignment_tracker[`${Object.keys(userInfoObject.assignment_tracker).length}`] = {
-                        "id": `${rows["insertId"]}`,
-                        "owned": "true"
-                    }
-                    console.log(userInfoObject);
-                    console.log(JSON.stringify(userInfoObject));
-                    const result = await dbPool.query(`UPDATE accounts SET info = '${JSON.stringify(userInfoObject)}' WHERE email = '${api.onlineUsers[socket.id].email}';`);
-
-                    for (let i = 0; i < Object.keys(api.onlineUsers).length; i++) {
-                        console.log(api.onlineUsers[Object.keys(api.onlineUsers)[i]].email, api.onlineUsers[socket.id].email);
-                        if (api.onlineUsers[Object.keys(api.onlineUsers)[i]].email === api.onlineUsers[socket.id].email) {
-                            await io.to(api.onlineUsers[Object.keys(api.onlineUsers)[i]].id).emit("assignment_tracker", {payload: "PULL_CLASSES_COMMAND"}, async (err, response) => {
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    if (response.status === "complete") {
-                                        console.log("completed command");
-                                    } else {
-                                        //failure
-                                    }
-                                }
-                            });
-                        }
-                    }
-                    return {status: "ok", payload: userInfoObject.assignment_tracker};
-                } catch(e) {
-                    console.log(e);
-                    return {status: "error", code: "Unknown error"};
-                }
-            } else {
-                return {status: "error", code: "Auth error"};
-            }
-            break;
-        default:
-            console.log("Assignment tracker socket io payload director failure");
-            return {status: "error", code: "Payload translation error"};
-    }
 }
 
 async function checklist_DIRECTOR(payload, socket) {
@@ -208,11 +93,6 @@ async function checklist_DIRECTOR(payload, socket) {
 function ioHandlers() {
     io.on('connection', (socket) => {
         console.log(`[${socket.id}] connected`);
-
-        socket.on('assignment_tracker', async (payload, callback) => {
-            let returnPayload = await assignment_tracker_DIRECTOR(payload, socket);
-            callback(returnPayload);
-        });
 
         socket.on('checklist', async (payload, callback) => {
             let returnPayload = await checklist_DIRECTOR(payload, socket);
